37549d22c8d629616b5123cc493f597d06e90198
fiach <fiach.reid@gmail.com>
2017-10-17 14:59:33 +0100

    Initial commit

---
+++ viesproxy.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using eu.europa.ec;
+using Newtonsoft.Json;
+
+
+public partial class viesproxy : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var svc = new checkVatService();
+        var countryCode = Request.QueryString["Country"];
+        var vatNumber = Request.QueryString["VAT"];
+        string name;
+        string address;
+        bool valid;
+        svc.checkVat(ref countryCode, ref vatNumber, out valid, out name, out address);
+        var data = new { name,  address};
+        var output = JsonConvert.SerializeObject(data);
+        Response.Write(output);
+    }
+}
